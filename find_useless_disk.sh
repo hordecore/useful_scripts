230514a390c5bc68f3fc06f45d1f6b1eece2773c
Oleg Strizhechenko <panzersoldat@mail.ru>
2013-01-21 18:06:16 +0600

    Update find_useless_disk.sh
    optimized output and filter

--- find_useless_disk.sh
+++ find_useless_disk.sh
@@ -4,17 +4,17 @@ TMPFILE=/tmp/$(basename $0).$$
 STORAGE=${1:-/qemu/img/}
 
 virsh list --all | tail -n +3 | while read num name state
-do 
-	if [ -n "$name" ]; then
-		virsh dumpxml $name | egrep -ow "/.*.(qcow2|qcow|img)"
-	fi
+do
+        if [ -n "$name" ]; then
+                virsh dumpxml $name | egrep -ow "/.*.(qcow2|qcow|img)"
+        fi
 done > "$TMPFILE"
 
-ls $STORAGE | grep -v *.xml | while read disk
+ls $STORAGE | grep -v *.xml | egrep  "(img|qcow|qcow2)" | while read disk
 do
-	if ! grep -q $disk $TMPFILE; then
-		echo -e "$(du -sh $STORAGE$disk | awk '{print $1}')\t$disk"
-	fi
-done 
+        if ! grep -q $disk $TMPFILE; then
+                echo -e "$(du -s $STORAGE$disk | awk '{print $1}')\t$disk"
+        fi
+done | sort -n
 
 rm -f $TMPFILE
