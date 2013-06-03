0565b06564c6fed21987577d9d3076d20fe7a892
Strizhechenko Oleg <oleg@carbonsoft.ru>
2013-06-03 11:00:50 +0600

    Чуть ускорил поиск машин с удалёнными дисками заменой grepов на fgrepы.

--- find_diskless_machines.sh
+++ find_diskless_machines.sh
@@ -4,4 +4,4 @@ while read tmp machine tmp; do
 	while read disk; do
 		[ ! -f "$disk" ] && echo $machine
 	done <<< "$(virsh dumpxml $machine | egrep -o "/.*(.img|.qcow|.qcow2)")"
-done <<< "$(virsh list --all | grep -A 10000 -- '---' | grep -v -- '---')" | sort -u
+done <<< "$(virsh list --all | fgrep -A 10000 -- '---' | fgrep -v -- '---')" | sort -u
