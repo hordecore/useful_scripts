5c727dda02ca718c2b1d878663f1a55f6161eb35
Oleg Strizhechenko <oleg@carbonsoft.ru>
2013-06-10 21:45:56 +0600

    Сделал получение списка CPU

--- multithread/mt.c
+++ multithread/mt.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/sysinfo.h>
 #include <pthread.h>
-
-int cpu_count = 2;
 
 int get_cpu_count(void)
 {
-	return __asm {
-		mov eax,01h
-		mov t,ebx
-	}
+	return get_nprocs();
 }
 
 void *func(void *arg)
 {
 	int i;
 	for (i = 0; i < 100000; i++)
-	{
 		printf("%d\n", i);
-	}
 	return;
 }
 
 int main(void)
 {
 	int i;
+	unsigned int cpu_count = get_cpu_count();
 	pthread_t t[cpu_count];
-	for (i = 0; i < cpu_count; i++) {
+
+	/* and no, you should not use one for-loop */
+	for (i = 0; i < cpu_count; i++) 
 		pthread_create(&t[i], NULL, func, NULL);
-	}
-	for (i = 0; i < cpu_count; i++) {
+	for (i = 0; i < cpu_count; i++) 
 		pthread_join(t[i], NULL);
-	}
 	return 0;
 }
